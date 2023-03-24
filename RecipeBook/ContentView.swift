4b01996102f268ac30249de970c67b750a29cad3
Callum Bennett <66990734+CyberTapir@users.noreply.github.com>
2023-03-24 15:03:45 +1100

    Initial Commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by Cally on 24/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
